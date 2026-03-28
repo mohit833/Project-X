3afe60d1da19117bc74966b6ad0f54cce94d4be7
Mohit M B <82676560+mohit833@users.noreply.github.com>
2026-03-28 20:21:31 +0530

    Add project files

---
+++ app/config.js
@@ -0,0 +1,6 @@
+export const APP_CONFIG = {
+  SUPABASE_URL: "https://bbsixbwihnoutukzybxo.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJic2l4YndpaG5vdXR1a3p5YnhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MDIzNjUsImV4cCI6MjA5MDI3ODM2NX0.-FhzFmxFv95keAuoSruBrbOh00aBIkD5qftCl-WqE5I",
+  DEMO_MODE: false,
+  APP_NAME: "IPL Prediction League",
+};
